471a3a6d30b209ddbfc35fb5f170868d2e5ed62f
LanceGiave96 <enricopedemonti@hotmail.it>
2023-10-07 17:30:03 +0200

    update blackjack with adding image of  cards on each draw

--- index.js
+++ index.js
@@ -14,15 +14,51 @@ let cardEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
 playerEl.textContent = player.name + ": €" + player.chips
 let chips = player.chips
+let cards_Section = document.getElementById("cards-Section")
+cards_Section.innerHTML = '<div id="cards-Section"></div>'
+let inner_Cards_Section = ""
 
 function getRandomCard()
 {   
     let randomNumber = Math.floor(Math.random() * 13) + 1
-    
+    let image_Path = ""
+
+    if(randomNumber<=10)
+    {
+        image_Path = "Cards-image/" + randomNumber + ".jpg"
+    }
+    else
+    {
+        switch(randomNumber)
+        {
+            
+            case 11:
+                image_Path = "Cards-image/Q.jpg"
+                break
+
+            case 12:
+                image_Path = "Cards-image/K.jpg"
+                break
+
+            case 13:
+                image_Path = "Cards-image/J.jpg"
+                break
+
+        }
+    }
+
+    inner_Cards_Section += '<img src="' + image_Path + '" class="card_Image">'
+    cards_Section.innerHTML = inner_Cards_Section
+
     if(randomNumber > 10)
         return 10
     else if(randomNumber === 1)
-        return 11
+
+        if(sum + 11 <= 21)
+            return 11
+        else
+            return 1
+
     else
         return randomNumber
 
@@ -32,14 +68,19 @@ function startGame()
 {   
     if(chips >= 10)
     {
-        
+
+        cards_Section.innerHTML = '<div id="cards-Section"></div>'
+        inner_Cards_Section = ""
+
         chips-= 10
         playerEl.textContent = player.name + ": €" + chips
         isAlive = true
+        hasBlackJack = false
         let firstCard = getRandomCard()
+        sum = firstCard
         let secondCard = getRandomCard()
+        sum += secondCard
         cards = [firstCard,secondCard]
-        sum = firstCard + secondCard
 
         renderGame()
 
